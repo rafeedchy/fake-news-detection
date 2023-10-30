14ef9ff5b76dc287742b8721ed9cb6e2fe11972e
MD RAFEEDUL BAR CHOWDHURY <rafeedchy94@gmail.com>
2023-10-30 22:05:22 +0200

    adding necessary packages

--- project.py
+++ project.py
@@ -1,1 +1,14 @@
-print('git upload')
+import numpy as np
+import pandas as pd
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+
+
+
+
+print('git upload and packages import')
