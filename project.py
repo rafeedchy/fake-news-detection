335557c67e3372f3363b43ab256e8c559486f2b0
MD RAFEEDUL BAR CHOWDHURY <rafeedchy94@gmail.com>
2023-10-31 16:01:42 +0200

    model train and test accuracy

--- project.py
+++ project.py
@@ -16,3 +16,20 @@ print(df.head())
 
 labels = df.label
 print(labels.head())
+
+x_train, x_test, y_train, y_test = train_test_split(df['text'], labels, test_size = 0.2, random_state = 7)
+
+
+#vectorizing text data
+tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+
+#fit and transform train, test set
+tfidf_train = tfidf_vectorizer.fit_transform(x_train)
+tfidf_test = tfidf_vectorizer.transform(x_test)
+
+model = PassiveAggressiveClassifier(max_iter=50)
+model.fit(tfidf_train, y_train)
+
+y_pred = model.predict(tfidf_test)
+score = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
